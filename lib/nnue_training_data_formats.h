5d07661ca947b63601013bad9181cbe5216e08d5
WWWishere <57116512+WWWishere@users.noreply.github.com>
2024-04-22 11:02:23 -0400

    dragon chess

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -57,8 +57,8 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define FILES 8
 #define RANKS 8
 #define PIECE_TYPES 7
-#define PIECE_COUNT 30
-#define POCKETS false
+#define PIECE_COUNT 34
+#define POCKETS true
 #define KING_SQUARES 64
 #define DATA_SIZE 512
 
