62cbd95ca18ab01a8d97130718283dd99e98dfc6
WWWishere <57116512+WWWishere@users.noreply.github.com>
2024-04-27 11:11:09 -0400

    hectochess

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -54,12 +54,12 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <intrin.h>
 #endif
 
-#define FILES 8
-#define RANKS 8
-#define PIECE_TYPES 7
-#define PIECE_COUNT 34
-#define POCKETS true
-#define KING_SQUARES 64
+#define FILES 10
+#define RANKS 10
+#define PIECE_TYPES 11
+#define PIECE_COUNT 52
+#define POCKETS false
+#define KING_SQUARES 100
 #define DATA_SIZE 512
 
 static_assert(DATA_SIZE % 8 == 0);
