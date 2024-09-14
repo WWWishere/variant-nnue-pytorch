d69e64ba4d40edc6385819a43213fe0b9382b99d
WWWishere <57116512+WWWishere@users.noreply.github.com>
2024-09-14 12:19:51 -0400

    imbalance var

--- lib/nnue_training_data_formats.h
+++ lib/nnue_training_data_formats.h
@@ -57,8 +57,8 @@ THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #define FILES 8
 #define RANKS 8
 #define PIECE_TYPES 7
-#define PIECE_COUNT 28
-#define POCKETS false
+#define PIECE_COUNT 33
+#define POCKETS true
 #define KING_SQUARES 64
 #define DATA_SIZE 512
 
